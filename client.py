587740e010af5a6080b0d51cb950b7d508d86132
J. Krajewski <brainpowerux@gmail.com>
2020-07-24 09:55:54 -0400

    Added comments

--- client.py
+++ client.py
@@ -1,15 +1,18 @@
+"""SOCKETCHAT by J. Krajewski, 2020, All rights reserved.
+Spin up server.py, connect to it with client.py and chat via the cli. 
+"""
 import os
 import sys
 import socket
 from threading import Thread
 
-BUFFSIZE = 4096
+BUFFSIZE = 4096  # Upped for encryption
 CURSOR_UP_ONE = '\x1b[1A'
 ERASE_LINE = '\x1b[2K'
 
 
 class Chime:
-
+    # Ring my bell, ring my bell
     def __init__(self):
         self.muted = False
 
@@ -22,70 +25,83 @@ class Chime:
 
 def welcome_msg():
 
-    # Get handle
+    # Sned @user handle
     while True:
         handle = input("Enter your handle:\n@")
         if handle:
             break
-
     client.send(handle.encode())
 
+    # Get message from server
     from_server = client.recv(BUFFSIZE)
     print(f"\x1b[4;32;40m{from_server.decode()}\x1b[0m")
 
 
 def receive():
+    # Incoming broadcasts!!
     while True:
         try:
             incoming = client.recv(BUFFSIZE)
             incoming = incoming.decode()
+
+            # Clear line when new text comes in (otherwise it'll glitch out.)
             sys.stdout.write(ERASE_LINE)
+
+            # Display with some sort of colors
+            print(f"\r\x1b[1;33;40m{incoming}\x1b[0m")
 
             # Bell
             chime.play()
-            print(f"\r\x1b[1;33;40m{incoming}\x1b[0m")
 
         except OSError:
             break
 
 
 def send(msg=''):
+    # Outgoing!!
     while msg != 'exit()':
         if msg == 'mute()':
             chime.muted = True
         elif msg == 'unmute()':
             chime.muted = False
+        else:
+            msg = input('')
+            client.send(msg.encode())
 
-        msg = input('')
-        client.send(msg.encode())
-
+    # Close on exit()
     client.close()
     print('Disconnected.')
     exit()
 
 
+# Instantiate sound
 chime = Chime()
 
 if __name__ == '__main__':
 
-    HOST = input('-+- Enter hostname of server: ')
-    if not HOST:
-        HOST = 'ubuntu'
+    host = input('-+- Enter hostname of server: ')
+    if not host:
+        # Set default
+        host = 'ubuntu'
 
-    PORT = input('-+- Choose port: ')
-    if not PORT:
-        PORT = 12222
+    port = input('-+- Choose port: ')
+    if not port:
+        # Set default
+        port = 12222
     else:
-        PORT = int(PORT)
+        port = int(port)
 
+    # Create client socket
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     try:
-        client.connect((HOST, PORT))  # Opens socket connect if server running.
+        client.connect((host, port))  # Opens socket connect if server running.
     except:
-        client.connect((HOST, PORT))  # Opens socket connect if server running.
+        client.connect((host, port))  # For debug
 
     welcome_msg()
 
+    # Start send/receive threads
     receive_thread = Thread(target=receive)
     receive_thread.start()
     send_thread = Thread(target=send)
